b7d604d0d6baa39b1b27a25bfa70fd060a3d468f
Selimhan <email@example.com>
2025-09-17 21:39:50 +0300

    config.pytamam

---
+++ config.py
@@ -0,0 +1,30 @@
+
+Kariyer = {
+    "Teknoloji": [
+        "Yazılım Geliştirici",
+        "Oyun Geliştirici",
+        "Veri Bilimci",
+        "Yapay Zeka Mühendisi"
+    ],
+    "Sanat": [
+        "Grafik Tasarımcı",
+        "Müzisyen",
+        "Video İçerik Üretici",
+        "Dijital Sanatçı"
+    ],
+    "İş Dünyası": [
+        "Girişimci",
+        "Pazarlama Uzmanı",
+        "Finans Analisti",
+        "Proje Yöneticisi"
+    ],
+    "Eğitim": [
+        "Öğretmen",
+        "Yaşam Koçu",
+        "Eğitim Danışmanı",
+        "Mentor"
+    ]
+}
+PREFIX ="!"
+
+TOKEN = ("")
