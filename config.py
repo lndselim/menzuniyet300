44260f55c3d7c1bf0b93c628ecc8d46e6033c683
Selimhan <email@example.com>
2025-09-24 09:27:25 +0300

    yeni meslekler eklendi

--- config.py
+++ config.py
@@ -1,30 +1,65 @@
-
 Kariyer = {
     "Teknoloji": [
         "Yazılım Geliştirici",
         "Oyun Geliştirici",
         "Veri Bilimci",
-        "Yapay Zeka Mühendisi"
+        "Yapay Zeka Mühendisi",
+        "Siber Güvenlik Uzmanı",
+        "Mobil Uygulama Geliştirici",
+        "Bulut Mühendisi",
+        "Donanım Mühendisi"
     ],
     "Sanat": [
         "Grafik Tasarımcı",
         "Müzisyen",
         "Video İçerik Üretici",
-        "Dijital Sanatçı"
+        "Dijital Sanatçı",
+        "Fotoğrafçı",
+        "Film Yönetmeni",
+        "Senarist",
+        "Animatör"
     ],
     "İş Dünyası": [
         "Girişimci",
         "Pazarlama Uzmanı",
         "Finans Analisti",
-        "Proje Yöneticisi"
+        "Proje Yöneticisi",
+        "İnsan Kaynakları Uzmanı",
+        "Satış Direktörü",
+        "Lojistik Yöneticisi",
+        "E-Ticaret Uzmanı"
     ],
     "Eğitim": [
         "Öğretmen",
         "Yaşam Koçu",
         "Eğitim Danışmanı",
-        "Mentor"
+        "Mentor",
+        "Psikolojik Danışman",
+        "Akademisyen",
+        "Özel Ders Eğitmeni",
+        "Online Kurs Eğitmeni"
+    ],
+    "Sağlık": [
+        "Doktor",
+        "Hemşire",
+        "Psikolog",
+        "Fizyoterapist",
+        "Diyetisyen",
+        "Eczacı",
+        "Cerrah",
+        "Paramedik"
+    ],
+    "Mühendislik": [
+        "Makine Mühendisi",
+        "Elektrik Mühendisi",
+        "İnşaat Mühendisi",
+        "Endüstri Mühendisi",
+        "Kimya Mühendisi",
+        "Biyomedikal Mühendis",
+        "Enerji Mühendisi",
+        "Uçak Mühendisi"
     ]
 }
-PREFIX ="!"
 
+PREFIX = "!"
 TOKEN = ("")
